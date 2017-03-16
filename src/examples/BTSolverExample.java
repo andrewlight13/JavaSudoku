996fa38cef669c31c0b0d9beb6fb91ff5a73dfab
palakhirani <phirani@uci.edu>
2017-03-16 10:57:56 -0700

    for running all combo

--- src/examples/BTSolverExample.java
+++ src/examples/BTSolverExample.java
@@ -13,41 +13,83 @@ public class BTSolverExample {
 	public static void main(String[] args)
 	{
 		//SudokuFile sf = SudokuBoardGenerator.generateBoard(9, 3, 3, 12);
-		SudokuFile sf = SudokuBoardReader.readFile("ExampleSudokuFiles/PH1.txt");
-		BTSolver solver = new BTSolver(sf);
+		String[] sudokuInputFiles1 = {"ExampleSudokuFiles/PE1.txt","ExampleSudokuFiles/PE2.txt","ExampleSudokuFiles/PE3.txt","ExampleSudokuFiles/PE4.txt","ExampleSudokuFiles/PE5.txt"};
+		String[] sudokuInputFilestoprint1 = {"PE1.txt","PE2.txt","PE3.txt","PE4.txt","PE5.txt"};
+		String[] sudokuInputFiles = {"ExampleSudokuFiles/PH1.txt","ExampleSudokuFiles/PH2.txt","ExampleSudokuFiles/PH3.txt","ExampleSudokuFiles/PH4.txt","ExampleSudokuFiles/PH5.txt"};
+		String[] sudokuInputFilestoprint = {"PH1.txt","PH2.txt","PM3.txt","PH4.txt","PH5.txt"};
+		//ConsistencyCheck[] consistencyChecks = {ConsistencyCheck.ForwardChecking, ConsistencyCheck.ArcConsistency};
+		ConsistencyCheck[] consistencyChecks = {ConsistencyCheck.ArcConsistency};
+
+		ValueSelectionHeuristic[] ValueSelection = {ValueSelectionHeuristic.None,ValueSelectionHeuristic.LeastConstrainingValue};
+		VariableSelectionHeuristic[] VariableSelection = {VariableSelectionHeuristic.None,VariableSelectionHeuristic.MinimumRemainingValue};
+		boolean[] Nakedp = {true, false};
+		boolean[] Nakedt = {true, false};
 		
-		solver.setConsistencyChecks(ConsistencyCheck.ForwardChecking);
-		solver.setValueSelectionHeuristic(ValueSelectionHeuristic.LeastConstrainingValue);
-		solver.setVariableSelectionHeuristic(VariableSelectionHeuristic.MinimumRemainingValue);
-		solver.setNakedPair(false);
-		solver.setNakedTriple(false);
-		
-		Thread t1 = new Thread(solver);
-		try
-		{
-			t1.start();
-			t1.join(60000);
-			if(t1.isAlive())
-			{
-				t1.interrupt();
+		for(int c=0; c < consistencyChecks.length; ++c) {
+			for(int val=0; val < ValueSelection.length; ++val){
+				for (int var=0;var < VariableSelection.length; ++var){
+					for(int np=0;np < Nakedp.length; ++np){
+						for(int nt=0;nt < Nakedt.length; ++nt){
+							System.out.println("\n\nConsistency Check: "+ consistencyChecks[c]+"\nValueSelectionHeuristic: "+ ValueSelection[val] + "\nVariableSelectionHeuristic: " + VariableSelection[var]+ "\nNaked Pair: " + Nakedp[np] + "\nNaked Triple: "+ Nakedt[nt]);
+							double timesum = 0.0;
+							double assignmentsum = 0.0;
+							double backtrackssum = 0.0;
+							for(int i=0; i < sudokuInputFiles1.length; ++i) {
+								SudokuFile sf = SudokuBoardReader.readFile(sudokuInputFiles1[i]);
+								//System.out.println(sf.toString());
+								BTSolver solver = new BTSolver(sf);
+								System.out.print(sudokuInputFilestoprint1[i]);
+								solver.setConsistencyChecks(consistencyChecks[c]);
+								solver.setValueSelectionHeuristic(ValueSelection[val]);
+								solver.setVariableSelectionHeuristic(VariableSelection[var]);
+								solver.setNakedPair(Nakedp[np]);
+								solver.setNakedTriple(Nakedt[nt]);
+								
+								Thread t1 = new Thread(solver);
+								try
+								{
+									t1.start();
+									t1.join(900000);
+									if(t1.isAlive())
+									{
+										t1.interrupt();
+									}
+								}catch(InterruptedException e)
+								{
+								}
+
+
+								if(solver.hasSolution())
+								{
+									solver.printSolverStats();
+									timesum += solver.getTimeTaken();
+									assignmentsum +=  solver.getNumAssignments();
+									backtrackssum +=  solver.getNumBacktracks();
+									//System.out.println(solver.getSolution());	
+									//System.out.println("Solved");
+
+								}
+
+								else
+								{
+									System.out.println("Failed to find a solution");
+								}
+							}
+							System.out.println("Average Solution Results: ");
+							System.out.println("Average runtime: "+ timesum/5 + " ms");
+							System.out.println("Average number of assignments per puzzle: " + assignmentsum/5);
+							System.out.println("Average number of backtracks per puzzle: " + backtrackssum/5);
+							
+							
+						}
+					}
+				}
 			}
-		}catch(InterruptedException e)
-		{
 		}
-
-
-		if(solver.hasSolution())
-		{
-			solver.printSolverStats();
-			System.out.println(solver.getSolution());	
-		}
-
-		else
-		{
-			System.out.println("Failed to find a solution");
-		}
-
+		
+		
 	}
+	
 }
 /*
 
@@ -76,6 +118,26 @@ Both
 Time taken:1384 ms
 Number of assignments: 185
 Number of backtracks: 2
+
+AC + LCV + MRV
+Time taken:20662 ms
+Number of assignments: 176
+Number of backtracks: 5
+
+naked pair + AC + LCV + MRV
+Time taken:23372 ms
+Number of assignments: 176
+Number of backtracks: 5
+
+both + AC +LCV +MRV
+Time taken:19013 ms
+Number of assignments: 182
+Number of backtracks: 3
+
+both tuples + LCV
+Failed to get result
+
+both tuples +LCV + MRV
 
 
 _________________________________
